5e4ff52d727dfedec7d6dc3702d6968c1f2e5a96
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2017-11-15 17:35:37 -0800

    mm: introduce wrappers to access mm->nr_ptes
    Let's add wrappers for ->nr_ptes with the same interface as for nr_pmd
    and nr_pud.
    
    The patch also makes nr_ptes accounting dependent onto CONFIG_MMU.  Page
    table accounting doesn't make sense if you don't have page tables.
    
    It's preparation for consolidation of page-table counters in mm_struct.
    
    Link: http://lkml.kernel.org/r/20171006100651.44742-1-kirill.shutemov@linux.intel.com
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 7fd517f0a3a1c0ccc81240210338ca8fe7a24579)
    (cherry picked from commit 0e2d856cb82682ede53f9b2c4e70bd719a20a204)
    (cherry picked from commit bc606ba48875f3fefb8c82abfaef11f74d3bab8b)
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1282,7 +1282,7 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 				/* assume page table is clear */
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
 				spin_unlock(ptl);
-				atomic_long_dec(&mm->nr_ptes);
+				mm_dec_nr_ptes(tlb->mm);
 				pte_free(mm, pmd_pgtable(_pmd));
 			}
 			up_write(&mm->mmap_sem);
