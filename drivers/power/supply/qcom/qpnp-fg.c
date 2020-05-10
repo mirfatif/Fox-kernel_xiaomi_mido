9d1f3a482d360b006c496cd5053ed19f1f80548c
AbhinandAK350 <abhinandak@outlook.com>
2020-05-10 16:03:02 +0530

    Fix 2% battery level drop

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -8118,11 +8118,7 @@ static int fg_common_hw_init(struct fg_chip *chip)
 	}
 
 	rc = fg_mem_masked_write(chip, settings[FG_MEM_DELTA_SOC].address, 0xFF,
-#ifdef CONFIG_MACH_XIAOMI_C6
-			1,
-#else
-			soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
-#endif
+		    settings[FG_MEM_DELTA_SOC].value == 1 ? 1 : soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
 			settings[FG_MEM_DELTA_SOC].offset);
 	if (rc) {
 		pr_err("failed to write delta soc rc=%d\n", rc);
