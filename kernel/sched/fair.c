49b375efdb93a4b5dbff82ee9de59e2cbd909ae7
ItsVixano <vixanonew@gmail.com>
2020-07-29 17:14:41 +0000

    sched/fair: Replace zero-length array with flexible-array
    Commit:
    =======
    
    The current codebase makes use of the zero-length array language extension to the C90
    standard, but the preferred mechanism to declare variable-length types such as these ones
    is a flexible array member[1][2],
    introduced in C99:
    
    struct foo {
            int stuff;
            struct boo array[];
    };
    
    By making use of the mechanism above, we will get a compiler warning in case the flexible
    array does not occur last in the structure, which will help us prevent some kind of
    undefined behavior bugs from being
    inadvertently introduced[3] to the codebase from now on.
    
    Also, notice that, dynamic memory allocations won't be affected by
    this change:
    
    "Flexible array members have incomplete type, and so the sizeof operator may not be
    applied. As a quirk of the original implementation of
    zero-length arrays, sizeof evaluates to zero."[1]
    
    sizeof(flexible-array-member) triggers a warning because flexible array members have
    incomplete type[1]. There are some instances of code in which the sizeof operator is being
    incorrectly/erroneously applied to zero-length arrays and the result is zero. Such
    instances may be hiding some bugs. So, this work (flexible-array member conversions) will
    also
    help to get completely rid of those sorts of issues.
    
    This issue was found with the help of Coccinelle.
    
    [1] https://gcc.gnu.org/onlinedocs/gcc/Zero-Length.html
    [2] KSPP/linux#21
    [3] commit 76497732932f ("cxgb3/l2t: Fix undefined behaviour")
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200507192141.GA16183@embeddedor
    Signed-off-by: celtare21 <celtare21@gmail.com>
    
    =======
    
    taken from https://github.com/celtare21/kernel_google_coral/commit/c369e8366c277b19f7da0ac52a16ed50b5c7275f
    
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1194,7 +1194,7 @@ struct numa_group {
 	 * more by CPU use than by memory faults.
 	 */
 	unsigned long *faults_cpu;
-	unsigned long faults[0];
+	unsigned long faults[];
 };
 
 /* Shared or private faults. */
